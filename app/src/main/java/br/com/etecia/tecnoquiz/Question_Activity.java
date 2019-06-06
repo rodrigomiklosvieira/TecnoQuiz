ec8000ec3fbeb8edace5a3189b4b5e08cba69a8f
rodrigomiklosvieira <rodrigo.vieira67@etec.sp.gov.br>
2019-06-05 22:14:08 -0300

    Iniciado app TecnoQuiz

---
+++ app/src/main/java/br/com/etecia/tecnoquiz/Question_Activity.java
@@ -0,0 +1,13 @@
+package br.com.etecia.tecnoquiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Question_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.question_layout);
+    }
+}
