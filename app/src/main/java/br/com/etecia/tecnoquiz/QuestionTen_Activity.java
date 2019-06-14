0d24cce9e6fefbee2b9b20d90585d9f434377dfa
rodrigomiklosvieira <rodrigo.vieira67@etec.sp.gov.br>
2019-06-14 19:46:12 -0300

    Finalizado as telas de questões.

---
+++ app/src/main/java/br/com/etecia/tecnoquiz/QuestionTen_Activity.java
@@ -0,0 +1,13 @@
+package br.com.etecia.tecnoquiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class QuestionTen_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.questionten_layout);
+    }
+}
