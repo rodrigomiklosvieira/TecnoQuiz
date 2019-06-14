735f3c8e204f19c85a01148421a04e22627ebd23
Paulo Henrique <paulo.oliveira438@etec.sp.gov.br>
2019-06-14 19:34:52 -0300

    Foram criadas as string das questões e foi dada função ao botão na Main Activity

--- app/src/main/java/br/com/etecia/tecnoquiz/MainActivity.java
+++ app/src/main/java/br/com/etecia/tecnoquiz/MainActivity.java
@@ -1,12 +1,20 @@
 package br.com.etecia.tecnoquiz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void entrarApp(View view) {
+
+        Intent intent = new Intent(MainActivity.this, Question_Activity.class);
+        startActivity(intent);
     }
 }
